42cc68becd55dd11d4440aa6b8a76ad2a3ec2e55
Bas Muhammed <bas.muhammed@fab4minds.com>
2026-08-24 16:05:42 +0200

    Added High-Availability Load-Balancer and auto-scaling EC2-instances

--- terraform/alb.tf
+++ terraform/alb.tf
@@ -1,36 +1,36 @@
 resource "aws_lb" "alb" {
-    name                = "Load-Balancer"
-    internal            = false
-    load_balancer_type  = "application"
-    subnets             = [aws_subnet.public.id, aws_subnet.public_2.id]
-    security_groups     = [aws_security_group.alb_sg.id]
+  name               = "Load-Balancer"
+  internal           = false
+  load_balancer_type = "application"
+  subnets            = [aws_subnet.public.id, aws_subnet.public_2.id]
+  security_groups    = [aws_security_group.alb_sg.id]
 }
 
 resource "aws_lb_target_group" "lb_target-group" {
-    name        = "target-group for the Load Balancer"
-    port        = 80
-    protocol    = "tcp"
-    vpc_id      = aws_vpc.main.id
+  name     = "web-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.main.id
 
-    health_check {
-        enabled             = true
-        path                = "/"
-        port                = traffic-port
-        protocol            = "tcp"
-        healthy_threshold   = 3
-        unhealthy_threshold = 3
-        timeout             = 5
-        interval            = 30
-    }
+  health_check {
+    enabled             = true
+    path                = "/"
+    port                = "traffic-port"
+    protocol            = "HTTP"
+    healthy_threshold   = 3
+    unhealthy_threshold = 3
+    timeout             = 5
+    interval            = 30
+  }
 }
 
-resource aws_lb_listener" "lb-listener" {
-    load_balancer_arn   = aws_lb.alb.arn
-    port                = 80
-    protocol            = "tcp"
+resource "aws_lb_listener" "lb-listener" {
+  load_balancer_arn = aws_lb.alb.arn
+  port              = 80
+  protocol          = "HTTP"
 
-    default_action {
-        type                = "forward"
-        target_group_arn    = aws_lb_target_group.lb_target-group.arn
-    }
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.lb_target-group.arn
+  }
 }
--- terraform/dynamodb.tf
+++ terraform/dynamodb.tf
@@ -1,9 +1,9 @@
 resource "aws_dynamodb_table" "users" {
-    name = "users"
-    billing_mode = "PAY_PER_REQUEST"
-    hash_key = "user_id"
-    attribute {
-        name = "user_id"
-        type = "S"
-    }
+  name         = "users"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "user_id"
+  attribute {
+    name = "user_id"
+    type = "S"
+  }
 }
